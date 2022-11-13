c56371c39fe715166259b455f0e1f2f3e6acd70a
Joan Hervás <hxrvas0708@gmail.com>
2022-11-13 13:48:33 +0100

    Added APA and LI Practica4, VC plate recognition improvement

--- VC/VC SP QT 2022/ShortProject.m
+++ VC/VC SP QT 2022/ShortProject.m
@@ -5,24 +5,34 @@ nf = size(a);
 figure
 for i = 1:nf 
     filename = horzcat(a(i).folder,'/',a(i).name);
+    name = a(i).name;
     I = imread(filename);
-    [h, w] = size(I);
-    gray = rgb2gray(I);
+    [~, w] = size(I);
+    gray = rgb2gray(I); 
     bin = platebin(gray);
     
     candidates = platecandidates(bin, w);
-    lookforplate(gray, candidates)
+    lookforplate(I, gray, candidates, name)
     pause
-end
+end 
+
+%%
+name = 'IMG_0415.jpg';
+I = imread(name);
+[h, w] = size(I);
+gray = rgb2gray(I);
+bin = platebin(gray);
+
+candidates = platecandidates(bin, w);
+lookforplate(I, gray, candidates, name)
+
 %%
 function res = platebin(im)
     clearborder = imclearborder(im);
     norm = mat2gray(clearborder);
     bin = imbinarize(norm);
     mark = imopen(bin, strel('disk', 5));
-    rec = imreconstruct(mark, bin);
-    rec = imerode(rec, 1);
-    res = imerode(rec, 1);
+    res = imreconstruct(mark, bin);
 end
 
 function candidates = platecandidates(bin, w)
@@ -66,28 +76,42 @@ function heights = boundingboxheights(bboxs)
 end
 
 function digits = platedigits(plate)
+    [h, w] = size(plate);
     plate = imcomplement(plate);
-    plate = imbinarize(plate);
-    plate = imclearborder(plate);
-    [labels, ~] = bwlabel(plate);
+    plate = imbinarize(adapthisteq(plate));
+    plate = blackborder(plate);
+    if w > 235
+        plate = imerode(plate, strel('disk',1));
+    end
+    [labels, ~] = bwlabel(plate, 4);
+    %figure, imshow(labels, []), colormap colorcube;
     props = regionprops(labels, 'BoundingBox');
     bboxs = cat(1, props.BoundingBox);
     heights = boundingboxheights(bboxs);
-    [h, ~] = size(plate);
-    digits = bboxs(heights >= 0.5*h, :, :, :);
+    widths = boundingboxwidths(bboxs);
+    condition = heights >= 0.4*h & widths <= 0.25 * w;
+    candidates = bboxs(condition, :, :, :);
+    [sz, ~] = size(candidates);
+    if sz >= 7
+        digits = candidates;
+    else
+        [labels, ~] = bwlabel(plate, 8);
+        %figure, imshow(labels, []), colormap colorcube;
+        props = regionprops(labels, 'BoundingBox');
+        bboxs = cat(1, props.BoundingBox);
+        heights = boundingboxheights(bboxs);
+        widths = boundingboxwidths(bboxs);
+        [h, w] = size(plate);
+        condition = heights >= 0.4*h & widths <= 0.25 * w;
+        digits = bboxs(condition, :, :, :);
+    end
 end
 
+
 function result = isaplate(plate)
-    plate = imcomplement(plate);
-    plate = imbinarize(plate);
-    plate = imclearborder(plate);
-    [labels, ~] = bwlabel(plate);
-    props = regionprops(labels, 'BoundingBox');
-    bboxs = cat(1, props.BoundingBox);
-    heights = boundingboxheights(bboxs);
-    [h, ~] = size(plate);
-    digits = bboxs(heights >= 0.5*h, :, :, :);
+    digits = platedigits(plate);
     [sz, ~] = size(digits);
+    %disp(sz)
     if sz >= 7
         result = true;
     else 
@@ -95,29 +119,40 @@ function result = isaplate(plate)
     end
 end
 
-function lookforplate(im, candidates)
+function lookforplate(I, gray, candidates, name)
     [sz, ~] = size(candidates);
     if sz > 0
         for i = 1:sz
             bbox = candidates(i, :, :, :);
-            plate = imcrop(im, bbox);
+            plate = imcrop(gray, bbox);
             if isaplate(plate)
                 digits = platedigits(plate);
                 [n, ~] = size(digits);
-                imshow(plate);
+                imshow(I), title(name);
                 hold on
+                rectangle('Position',bbox,'EdgeColor','y')
                 for j = 1:n
-                    bbox = digits(j, :, :, :);
-                    rectangle('Position',bbox,'EdgeColor','g')
+                    digit = digits(j, :, :, :);
+                    digit(1) = digit(1) + bbox(1);
+                    digit(2) = digit(2) + bbox(2);
+                    rectangle('Position',digit,'EdgeColor','g')
                 end
                 hold off
                 break
-            %else 
-                %disp("NOT A PLATE")
+            elseif i == sz
+                disp("NO PLATE FOUND IN IMAGE "+name)
             end
         end
     else
-        disp("NO CANDIDATES FOUND")
+        disp("NO CANDIDATES FOUND IN IMAGE "+name)
     end
+end
+
+function im = blackborder(im)
+    [n, m] = size(im);
+    im(1 , :) = 0;
+    im(n, :) = 0;
+    im(:, 1) = 0;
+    im(:, m) = 0;
 end
 
